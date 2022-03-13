139af8c0e7de4e1d5ece5422d0df7b3c93e01118
blc <blc@blcs-air.attlocal.net>
2022-03-12 19:48:04 -0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+	public static void main(String[] args){
+		int x = 0;
+		int s = 0;
+		while(x < 10) {
+			s += x;
+			System.out.print(s + "10");
+			x = x + 1;
+
+		}
+	}
+}
